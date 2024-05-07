5e89d2e06a9ec3c7c1b7e236f59efae490c435d3
aryasasil00179434 <l00179434@atu.ie>
2024-05-07 15:25:19 +0100

    initital commit

---
+++ regress.R
@@ -0,0 +1,48 @@
+var<-lm(weight ~ height,data=women)
+var
+summary(var)
+View(women)
+#y=b0+b1*x
+#weight~ -87.52+3.43*height
+
+cars<-cars
+View(cars)
+summary(cars)
+
+windows(20,12)
+scatter.smooth(x=cars$speed,
+               y=cars$dist,main="Dist~Speed")
+windows(20,12)
+par(mfrow=c(1,2))
+#its for checking varience
+boxplot(cars$speed,main="BOX PLOT- Speed")
+boxplot(cars$dist,main="BOX Plot -Distance")
+#here varience is not study..so no linear regression
+
+
+windows(20,16)
+par(mfrow=c(1,2))
+plot(density(cars$speed),
+     main="Density Plot:Speed",
+     ylab="Frequency")
+polygon(density(cars$speed),col="blue")
+
+plot(density(cars$dist),
+     main="Density Plot:Distance",
+     ylab="Frequency")
+polygon(density(cars$dist),col="red")
+
+#Correlation
+cor(cars$speed,cars$dist)
+#correlation matrix
+cor(cars)
+
+
+
+#linear Regression
+attach(cars)
+linearmodel<-lm(dist ~ speed)
+linearmodel
+
+
+##
